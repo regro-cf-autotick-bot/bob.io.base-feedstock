464c829b409cc0ff8b102eb7fb9b7e5386988122
Amir Mohammadi <183.amir@gmail.com>
2016-10-22 10:41:50 +0200

    Update to version 2.0.12

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -47,40 +47,40 @@ source run_conda_forge_build_setup
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
     export HDF5_VERSION="1.8.17|1.8.17.*"
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
     export HDF5_VERSION="1.8.16|1.8.16.*"
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
     export HDF5_VERSION="1.8.17|1.8.17.*"
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
     export HDF5_VERSION="1.8.16|1.8.16.*"
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
     set -x
     export HDF5_VERSION="1.8.17|1.8.17.*"
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 EOF
